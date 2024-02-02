13d35a711dd2fb1eb7b041341da56553f68913d9
J0nathan550 <ihudokormov@gmail.com>
2024-02-02 14:51:16 +0100

    Add project files.

---
+++ Advanced Rich Presence/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Advanced_Rich_Presence.Windows;
+using Microsoft.UI.Xaml;
+
+namespace Advanced_Rich_Presence
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+        }
+
+        private Window m_window;
+    }
+}
---
+++ Advanced Rich Presence/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace Advanced_Rich_Presence.Windows
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
