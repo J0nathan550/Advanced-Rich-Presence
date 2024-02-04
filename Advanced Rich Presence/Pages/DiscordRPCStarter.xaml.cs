6b42950b3a8b4a26c2b699850032b0845e13fed8
J0nathan550 <ihudokormov@gmail.com>
2024-02-04 02:48:29 +0100

    Added fonts, and added little bit of visuals

--- Advanced Rich Presence/Pages/DiscordRPCStarter.xaml.cs
+++ Advanced Rich Presence/Pages/DiscordRPCStarter.xaml.cs
@@ -5,28 +5,37 @@ namespace Advanced_Rich_Presence.Pages;
 
 public partial class DiscordRPCStarter : Page
 {
-    public DiscordRpcClient client;
+    //public DiscordRpcClient client;
 
     public DiscordRPCStarter()
     {
         InitializeComponent();
-        client = new DiscordRpcClient("1056335832518250496");
+        //client = new DiscordRpcClient("1056335832518250496");
 
-        //Connect to the RPC
-        client.Initialize();
+        ////Connect to the RPC
+        //client.Initialize();
 
-        //Set the rich presence
-        //Call this as many times as you want and anywhere in your code.
-        client.SetPresence(new RichPresence()
+        ////Set the rich presence
+        ////Call this as many times as you want and anywhere in your code.
+        //client.SetPresence(new RichPresence()
+        //{
+        //    Details = "Example Project",
+        //    State = "csharp example",
+        //    Assets = new Assets()
+        //    {
+        //        LargeImageKey = "image_large",
+        //        LargeImageText = "Lachee's Discord IPC Library",
+        //        SmallImageKey = "image_small"
+        //    }
+        //});
+    }
+
+    private void CreateTemplateButton_Clicked(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if (MainWindow.mainWindow != null)
         {
-            Details = "Example Project",
-            State = "csharp example",
-            Assets = new Assets()
-            {
-                LargeImageKey = "image_large",
-                LargeImageText = "Lachee's Discord IPC Library",
-                SmallImageKey = "image_small"
-            }
-        });
+            if (MainWindow.mainWindow.mainPageView.CurrentSourcePageType == typeof(CreateDiscordRPCStatus)) return;
+            MainWindow.mainWindow.mainPageView.Navigate(typeof(CreateDiscordRPCStatus));
+        }
     }
 }
---
+++ Advanced Rich Presence/Pages/EditDiscordRPCStatus.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Advanced_Rich_Presence.Pages
+{
+    public partial class EditDiscordRPCStatus : Page
+    {
+        public EditDiscordRPCStatus()
+        {
+            InitializeComponent();
+        }
+    }
+}
